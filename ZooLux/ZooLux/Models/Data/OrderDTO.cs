39a3a63c3d7401e9549c5b3aaad2c48ddd5773ff
Andriy Halelyuka <andriyhalelyuka@ukr.net>
2021-03-25 17:07:06 +0200

    Project preparation, writing ViewModels and preparation for database integration

---
+++ ZooLux/ZooLux/Models/Data/OrderDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZooLux.Models.Data
+{
+    [Table("Orders")]
+    public class OrderDTO
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual UserDTO Users { get; set; }
+    }
+}
