492e17a8acd99292cd80fab1d75c81160285e88f
Andriy Halelyuka <43058552+andreydef@users.noreply.github.com>
2021-03-25 22:27:51 +0200

    Project preparation, writing Models and preparation for database integration

---
+++ ZooLux/ZooLux/Models/Data/RoleDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ZooLux.Models.Data
+{
+    [Table("Roles")]
+    public class RoleDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
