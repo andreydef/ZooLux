7c61777a1e271022db95544579890568c3228f1f
Andriy Halelyuka <43058552+andreydef@users.noreply.github.com>
2021-03-25 21:55:03 +0200

    Writing all ViewModels

---
+++ ZooLux/ZooLux/Models/ViewModels/Pages/PageVM.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using ZooLux.Models.Data;
+
+namespace ZooLux.Models.ViewModels.Pages
+{
+    public class PageVM
+    {
+        public PageVM() { }
+
+        public PageVM(PagesDTO row)
+        {
+            Id = row.Id;
+            Title = row.Title;
+            Slug = row.Slug;
+            Body = row.Body;
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        public string Title { get; set; }
+
+        public string Slug { get; set; }
+
+        [Required]
+        [StringLength(int.MaxValue, MinimumLength = 3)]
+        [AllowHtml]
+        public string Body { get; set; }
+
+        public IEnumerable<SelectListItem> Pages { get; set; }
+    }
+}
---
+++ ZooLux/ZooLux/Models/ViewModels/Shop/ProductVM.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+using ZooLux.Models.Data;
+
+namespace ZooLux.Models.ViewModels.Shop
+{
+    public class ProductVM
+    {
+        public ProductVM() { }
+
+        public ProductVM(ProductDTO row)
+        {
+            Id = row.Id;
+            Name = row.Name;
+            Slug = row.Slug;
+            Description = row.Description;
+            Price = row.Price;
+            CategoryName = row.CategoryName;
+            CategoryId = row.CategoryId;
+            ImageName = row.ImageName;
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Slug { get; set; }
+
+        [AllowHtml]
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string CategoryName { get; set; }
+
+        [Required]
+        [DisplayName("Category")]
+        public int CategoryId { get; set; }
+
+        [DisplayName("Image")]
+        public string ImageName { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        public IEnumerable<string> GalleryImages { get; set; }
+    }
+}
