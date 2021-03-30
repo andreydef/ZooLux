4c876e031aadb61441e116ae5d902f24acd0b66d
Andriy Halelyuka <43058552+andreydef@users.noreply.github.com>
2021-03-30 20:30:40 +0300

    Adding some changes in design folder and updating main admin area page

---
+++ ZooLux/ZooLux/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ZooLux.Areas.Admin.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Admin/Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
