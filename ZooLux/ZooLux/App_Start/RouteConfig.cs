13275be62ae591a07b3c7337e3e03180bc3d9e7c
Andriy Halelyuka <43058552+andreydef@users.noreply.github.com>
2021-04-18 18:51:16 +0300

    Work with admin area and create main page of a site
    Design admin dashboard
    Create main page of a site
    Update styles of admin area
    Update routings

--- ZooLux/ZooLux/App_Start/RouteConfig.cs
+++ ZooLux/ZooLux/App_Start/RouteConfig.cs
@@ -9,11 +9,18 @@ namespace ZooLux
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" },
+                new[] { "ZooLux.Controllers" });
+
+            routes.MapRoute("Default", "",
+                new { controller = "Pages", action = "Index" }, 
+                new[] { "ZooLux.Controllers" });
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
