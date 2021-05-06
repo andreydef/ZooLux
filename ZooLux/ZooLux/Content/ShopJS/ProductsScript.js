dff3ca868c04dfe39070d5514ba762b51117246b
Tali Khanas <47400159+Tali32@users.noreply.github.com>
2021-05-06 23:34:16 +0300

    add files

---
+++ ZooLux/ZooLux/Content/ShopJS/ProductsScript.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    /* Select product from specified category */
+    $("#SelectCategory").on("change", function () {
+        var url = $(this).val();
+
+        if (url) {
+            window.location = "/admin/shop/Products?catId=" + url;
+        }
+        return false;
+    });
+
+    /* Confirm product deletion */
+    $("a.delete").click(function () {
+        if (!confirm("Confirm product deletion")) return false;
+    });
+}); 
